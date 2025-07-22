d354d9f7f97ae35911533f9fccb872a592630dc2
. <sdm01@kenvue.com>
2025-07-22 17:10:57 +0530

    chore(release): ts ignore

--- components/OfficeFloorPlan.tsx
+++ components/OfficeFloorPlan.tsx
@@ -2,7 +2,8 @@
 
     import { useEffect, useRef, useState, useCallback } from "react";
     import * as THREE from "three";
-    import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+    // @ts-ignore
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
     import {
     Search,
     Home,
